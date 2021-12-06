903da2b16671d969b7457636806cb8b298f00dbf
jaem1n207 <roy.jm.lee@gmail.com>
2021-12-07 01:40:47 +0900

    Add: color palette

--- src/types/theme.d.ts
+++ src/types/theme.d.ts
@@ -4,6 +4,17 @@ declare module '@emotion/react' {
   export interface Theme {
     colors: {
       background: string;
+      searchToolbar: string;
+      icon: {
+        default: string;
+      };
+      vote: {
+        holderUnChecked: string;
+        holderChecked: string;
+        unchecked: string;
+        checked: string;
+      };
+      divider: string;
     };
     device: {
       mobile: string;
